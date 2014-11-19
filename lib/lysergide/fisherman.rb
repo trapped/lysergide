3065e587f229b58247d0ba06ffee0f4f072ec768
trapped <giorgio.pellero@gmail.com>
2014-11-19 22:14:42 +0100

    Faraday handle class -> instance variable in Fisherman

--- lib/lysergide/fisherman.rb
+++ lib/lysergide/fisherman.rb
@@ -2,7 +2,7 @@ require 'faraday'
 require 'faraday_middleware'
 
 module Fisherman
-	@@faraday = Faraday.new(:url => 'https://api.github.com') do |faraday|
+	@faraday = Faraday.new(:url => 'https://api.github.com') do |faraday|
 		faraday.request  :json
 		faraday.response :json
 		faraday.adapter  Faraday.default_adapter
