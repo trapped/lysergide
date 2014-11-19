00a7cafd9ac15eb2a248f27178bc9efd3150fa72
trapped <giorgio.pellero@gmail.com>
2014-11-19 22:39:53 +0100

    Static files to separate 'static' folder

--- lib/lysergide/application.rb
+++ lib/lysergide/application.rb
@@ -6,13 +6,6 @@ require 'coffee'
 class Lysergide::Application < Sinatra::Base
 	set :server, :webrick
 	set :views, settings.root + '/../../views'
-
-	# Static images
-	get '/img/*' do |path|
-		if File.exists? settings.root + "/../../images/#{path}"
-			send_file settings.root + "/../../images/#{path}" 
-		else
-			status 404
-		end
-	end
+	set :public_folder, settings.root + '/../../static'
+	enable :static
 end
