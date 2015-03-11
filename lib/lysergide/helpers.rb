afd48ac7edd68e524a058722c18487ee568a9530
trapped <giorgio@pellero.it>
2015-03-11 19:13:47 +0100

    Add Lysergide::Helpers

---
+++ lib/lysergide/helpers.rb
@@ -0,0 +1,18 @@
+module Lysergide::Helpers
+	def self.duration(num)
+		secs  = num.to_int
+		mins  = secs / 60
+		hours = mins / 60
+		days  = hours / 24
+
+		if days > 0
+			"#{days} days and #{hours % 24} hours"
+		elsif hours > 0
+			"#{hours} hours and #{mins % 60} minutes"
+		elsif mins > 0
+			"#{mins} minutes and #{secs % 60} seconds"
+		elsif secs >= 0
+			"#{secs} seconds"
+		end
+	end
+end
