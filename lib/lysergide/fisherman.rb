b5493370124fcc5ba209bbe9766642283c1945de
trapped <giorgio@pellero.it>
2015-03-12 17:23:10 +0100

    GitHub hook in Fisherman

--- lib/lysergide/fisherman.rb
+++ lib/lysergide/fisherman.rb
@@ -1,5 +1,8 @@
-require 'sinatra/base'
 require 'lysergide/database'
+require 'sinatra/base'
+require 'json'
+require 'openssl'
+require 'rack/utils'
 
 include Lysergide::Database
 
@@ -32,6 +35,54 @@ class Lysergide::Fisherman < Sinatra::Base
 					status 200
 				else
 					status 500
+				end
+			end
+		else
+			status 400
+		end
+	end
+
+	# GitHub (JSON) hook
+	post '/hook/github' do
+		request.body.rewind
+		body = request.body.read
+		gh_payload = JSON.parse(body)
+		case request.env['HTTP_X_GITHUB_EVENT']
+		when 'ping'
+			LOG.info('Lysergide::Fisherman') { "Received GitHub ping event (#{gh_payload['zen']}/#{gh_payload['hook_id']})" }
+			status 200
+		when 'push'
+			commits = gh_payload['commits']
+			gh_repo = gh_payload['repository']
+			repos = Repo.where(remote: [gh_repo['url'], gh_repo['clone_url'], gh_repo['git_url'], gh_repo['ssh_url']])
+			repos.each do |repo|
+				unique_signature = 'sha1=' + OpenSSL::HMAC.hexdigest(OpenSSL::Digest.new('sha1'), repo.token, body)
+				if Rack::Utils.secure_compare(unique_signature, request.env['HTTP_X_HUB_SIGNATURE'])
+					commits.each do |commit|
+						new_commit = commit['sha'] || commit['id']
+						if !new_commit || new_commit.empty?
+							status 500
+						end
+						last_build = repo.builds.order(number: :desc).first
+						number = 1
+						if last_build
+							number = last_build.number + 1
+						end
+						new_build = repo.builds.create({
+							user_id: repo.user.id,
+							number: number,
+							ref: new_commit,
+							status: scheduled
+						})
+						if new_build
+							LOG.info('Lysergide::Fisherman') {
+								"New build (#{new_build.repo.user.name}/#{new_build.repo.name}##{new_build.number}) has been scheduled"
+							}
+							status 200
+						else
+							status 500
+						end
+					end
 				end
 			end
 		else
