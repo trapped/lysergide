8cc5b8021eb809960ce52c479bcd2b20e7f7b3a1
trapped <giorgio@pellero.it>
2015-05-07 10:39:01 +0200

    Space alignment

--- lib/lysergide/database.rb
+++ lib/lysergide/database.rb
@@ -5,42 +5,42 @@ module Lysergide
   module Database
     # Connect to the database
     ActiveRecord::Base.establish_connection ({
-      adapter: 'sqlite3',
+      adapter:  'sqlite3',
       database: 'lysergide.db',
-      pool: 1024
+      pool:     1024
     })
 
     # Define the database schema
     ActiveRecord::Schema.define do
       unless ActiveRecord::Base.connection.tables.include? 'users'
         create_table :users do |table|
-          table.column :name,     :string
-          table.column :email,    :string
-          table.column :password,   :string
+          table.column :name,          :string
+          table.column :email,         :string
+          table.column :password,      :string
         end
       end
 
       unless ActiveRecord::Base.connection.tables.include? 'repos'
         create_table :repos do |table|
-          table.column :name,     :string
-          table.column :import_path,  :string
-          table.column :user_id,    :integer
-          table.column :token,    :string
-          table.column :last_pull,  :string
-          table.column :pull_interval,:integer
+          table.column :name,          :string
+          table.column :import_path,   :string
+          table.column :user_id,       :integer
+          table.column :token,         :string
+          table.column :last_pull,     :string
+          table.column :pull_interval, :integer
         end
       end
 
       unless ActiveRecord::Base.connection.tables.include? 'builds'
         create_table :builds do |table|
-          table.column :repo_id,    :integer
-          table.column :user_id,    :integer
-          table.column :number,     :integer
-          table.column :status,     :text
-          table.column :date,       :string
-          table.column :duration,   :integer
-          table.column :ref,        :text
-          table.column :log,        :text
+          table.column :repo_id,       :integer
+          table.column :user_id,       :integer
+          table.column :number,        :integer
+          table.column :status,        :text
+          table.column :date,          :string
+          table.column :duration,      :integer
+          table.column :ref,           :text
+          table.column :log,           :text
         end
       end
     end
@@ -52,7 +52,7 @@ module Lysergide
 
     class Repo < ActiveRecord::Base
       belongs_to :user
-      has_many :builds
+      has_many   :builds
     end
 
     class Build < ActiveRecord::Base
